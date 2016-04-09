3a7fb30dde160bc31c0bba4f42f7666ea6ca054a
Ryan Trinkle <ryan.trinkle@gmail.com>
2016-04-09 19:16:47 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,17 +1,24 @@
-{ mkDerivation, dependent-map, dependent-sum
-, mtl, ref-tf, semigroups, these, MemoTrie, exception-transformers
-, haskell-src-exts, haskell-src-meta
+{ mkDerivation, base, containers, dependent-map, dependent-sum
+, exception-transformers, haskell-src-exts, haskell-src-meta
+, MemoTrie, mtl, primitive, ref-tf, semigroups, stdenv, syb
+, template-haskell, these, transformers, transformers-compat
+, criterion, deepseq, split, stm
 }:
 mkDerivation {
   pname = "reflex";
   version = "0.4.0";
   src = builtins.filterSource (path: type: baseNameOf path != ".git") ./.;
-  buildDepends = [
-    dependent-map dependent-sum mtl ref-tf semigroups these exception-transformers
-    haskell-src-exts haskell-src-meta
+  libraryHaskellDepends = [
+    base containers dependent-map dependent-sum exception-transformers
+    haskell-src-exts haskell-src-meta mtl primitive ref-tf semigroups
+    syb template-haskell these transformers transformers-compat
   ];
-  testDepends = [
-    MemoTrie
+  testHaskellDepends = [
+    base containers dependent-map MemoTrie mtl ref-tf
+    # Benchmark dependencies
+    criterion deepseq split stm
   ];
-  license = null;
+  homepage = "https://github.com/reflex-frp/reflex";
+  description = "Higher-order Functional Reactive Programming";
+  license = stdenv.lib.licenses.bsd3;
 }
