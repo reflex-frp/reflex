d24121cb01a39593afb42625392eea6711e2ed08
Ryan Trinkle <ryan.trinkle@gmail.com>
2016-08-09 16:26:29 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,24 +1,22 @@
-{ mkDerivation, base, containers, dependent-map, dependent-sum
-, exception-transformers, haskell-src-exts, haskell-src-meta
-, MemoTrie, mtl, primitive, ref-tf, semigroups, stdenv, syb
-, template-haskell, these, transformers, transformers-compat
-, criterion, deepseq, split, stm, loch-th, hlint
+{ mkDerivation, base, bifunctors, containers, deepseq
+, dependent-map, dependent-sum, exception-transformers
+, haskell-src-exts, haskell-src-meta, hlint, MemoTrie, mtl
+, primitive, ref-tf, semigroupoids, semigroups, split, stdenv, stm
+, syb, template-haskell, these, transformers, transformers-compat
 }:
 mkDerivation {
   pname = "reflex";
   version = "0.4.0";
   src = builtins.filterSource (path: type: baseNameOf path != ".git") ./.;
   libraryHaskellDepends = [
-    base containers dependent-map dependent-sum exception-transformers
-    haskell-src-exts haskell-src-meta mtl primitive ref-tf semigroups
-    syb template-haskell these transformers transformers-compat
+    base bifunctors containers dependent-map dependent-sum
+    exception-transformers haskell-src-exts haskell-src-meta MemoTrie
+    mtl primitive ref-tf semigroupoids semigroups stm syb
+    template-haskell these transformers transformers-compat
   ];
   testHaskellDepends = [
-    base containers dependent-map MemoTrie mtl ref-tf
-    # Benchmark dependencies
-    criterion deepseq split stm loch-th
-    # HLint
-    hlint
+    base bifunctors containers deepseq dependent-map dependent-sum
+    hlint mtl ref-tf split transformers
   ];
   homepage = "https://github.com/reflex-frp/reflex";
   description = "Higher-order Functional Reactive Programming";
