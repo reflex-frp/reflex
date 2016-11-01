cfe46ed43b2a8164a39576a9489e65e1d87952cc
Ryan Trinkle <ryan.trinkle@gmail.com>
2016-11-01 19:10:09 -0400

    Expose distributeMapOverDynPure

--- src/Reflex/Dynamic.hs
+++ src/Reflex/Dynamic.hs
@@ -38,6 +38,7 @@ module Reflex.Dynamic
        , traceDyn
        , traceDynWith
        , splitDynPure
+       , distributeMapOverDynPure
        , distributeDMapOverDynPure
        , Demux
        , demux
