d7e1f98aeb186a38622378a0d4a30612c5d6034c
Doug Beardsley <mightybyte@gmail.com>
2017-03-15 16:05:33 -0400

    Add MonadPostpone instances for ReaderT and WriterT

--- src/Reflex/Postpone/Class.hs
+++ src/Reflex/Postpone/Class.hs
@@ -1,6 +1,17 @@
 module Reflex.Postpone.Class where
 
+import Control.Monad.Trans.Control
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.Writer
+
 class Monad m => MonadPostpone m where
   -- | Postpone an action until all containing 'mfix'es can be expected to have
   -- finished executing
   postpone :: m a -> m a
+
+instance MonadPostpone m => MonadPostpone (ReaderT r m) where
+  postpone = liftThrough postpone
+
+instance (MonadPostpone m, Monoid w) => MonadPostpone (WriterT w m) where
+  postpone = liftThrough postpone
+
