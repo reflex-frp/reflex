7cc49e60d75cd5b4ce70bb3fd1be73fc2bfdb8a5
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-05-28 16:10:57 -0400

    Make EventWriterT only require Semigroup, not Monoid

--- src/Reflex/EventWriter.hs
+++ src/Reflex/EventWriter.hs
@@ -38,7 +38,7 @@ import qualified Data.Dependent.Map as DMap
 import Data.Foldable
 import Data.Functor.Compose
 import Data.Functor.Misc
-import qualified Data.List.NonEmpty as NonEmpty
+import Data.List.NonEmpty (NonEmpty (..))
 import Data.Map (Map)
 import Data.Semigroup
 import Data.Sequence
@@ -47,7 +47,7 @@ import Data.Tuple
 
 -- | 'EventWriter' efficiently collects 'Event' values using 'tellEvent'
 -- and combines them monoidally to provide an 'Event' result.
-class (Monad m, Monoid w) => EventWriter t w m | m -> t w where
+class (Monad m, Semigroup w) => EventWriter t w m | m -> t w where
   tellEvent :: Event t w -> m ()
 
 -- | A basic implementation of 'EventWriter'.
@@ -60,7 +60,7 @@ runEventWriterT (EventWriterT a) = do
   (result, requests) <- runStateT a mempty
   return (result, mconcat $ toList requests)
 
-instance (Reflex t, Monad m, Monoid w) => EventWriter t w (EventWriterT t w m) where
+instance (Reflex t, Monad m, Semigroup w) => EventWriter t w (EventWriterT t w m) where
   tellEvent w = EventWriterT $ modify (|> w)
 
 instance EventWriter t w m => EventWriter t w (ReaderT r m) where
@@ -77,7 +77,7 @@ instance MonadHold t m => MonadHold t (EventWriterT t w m) where
   holdDyn v0 = lift . holdDyn v0
   holdIncremental v0 = lift . holdIncremental v0
 
-instance (Reflex t, MonadAdjust t m, MonadHold t m, Monoid w, Semigroup w) => MonadAdjust t (EventWriterT t w m) where
+instance (Reflex t, MonadAdjust t m, MonadHold t m, Semigroup w, Semigroup w) => MonadAdjust t (EventWriterT t w m) where
   runWithReplace = runWithReplaceEventWriterTWith $ \dm0 dm' -> lift $ runWithReplace dm0 dm'
   traverseDMapWithKeyWithAdjust = sequenceDMapWithAdjustEventWriterTWith (\f dm0 dm' -> lift $ traverseDMapWithKeyWithAdjust f dm0 dm') mapPatchDMap weakenPatchDMapWith patchMapNewElements mergeMapIncremental
   traverseDMapWithKeyWithAdjustWithMove = sequenceDMapWithAdjustEventWriterTWith (\f dm0 dm' -> lift $ traverseDMapWithKeyWithAdjustWithMove f dm0 dm') mapPatchDMapWithMove weakenPatchDMapWithMoveWith patchMapWithMoveNewElements mergeMapIncrementalWithMove
@@ -92,7 +92,7 @@ instance Requester t m => Requester t (EventWriterT t w m) where
 -- 'runWithReplace' for 'EventWriterT'.  This is necessary when the underlying
 -- monad doesn't have a 'MonadAdjust' instance or to override the default
 -- 'MonadAdjust' behavior.
-runWithReplaceEventWriterTWith :: forall m t w a b. (Reflex t, MonadHold t m, Monoid w)
+runWithReplaceEventWriterTWith :: forall m t w a b. (Reflex t, MonadHold t m, Semigroup w)
                                => (forall a' b'. m a' -> Event t (m b') -> EventWriterT t w m (a', Event t b'))
                                -> EventWriterT t w m a
                                -> Event t (EventWriterT t w m b)
@@ -101,14 +101,14 @@ runWithReplaceEventWriterTWith f a0 a' = do
   let g :: EventWriterT t w m c -> m (c, Seq (Event t w))
       g (EventWriterT r) = runStateT r mempty
   (result0, result') <- f (g a0) $ fmap g a'
-  request <- holdDyn (fmapCheap (mconcat . NonEmpty.toList) $ mergeList $ toList $ snd result0) $ fmapCheap (fmapCheap (mconcat . NonEmpty.toList) . mergeList . toList . snd) result'
+  request <- holdDyn (fmapCheap sconcat $ mergeList $ toList $ snd result0) $ fmapCheap (fmapCheap sconcat . mergeList . toList . snd) result'
   -- We add these two separately to take advantage of the free merge being done later.  The coincidence case must come first so that it has precedence if both fire simultaneously.  (Really, we should probably block the 'switch' whenever 'updated' fires, but switchPromptlyDyn has the same issue.)
   EventWriterT $ modify $ flip (|>) $ coincidence $ updated request
   EventWriterT $ modify $ flip (|>) $ switch $ current request
   return (fst result0, fmapCheap fst result')
 
 -- | Like 'runWithReplaceEventWriterTWith', but for 'sequenceDMapWithAdjust'.
-sequenceDMapWithAdjustEventWriterTWith :: forall t m p p' w k v v'. (Reflex t, MonadHold t m, Monoid w, Semigroup w, Patch (p' (Some k) (Event t w)), PatchTarget (p' (Some k) (Event t w)) ~ Map (Some k) (Event t w))
+sequenceDMapWithAdjustEventWriterTWith :: forall t m p p' w k v v'. (Reflex t, MonadHold t m, Semigroup w, Semigroup w, Patch (p' (Some k) (Event t w)), PatchTarget (p' (Some k) (Event t w)) ~ Map (Some k) (Event t w))
                                        => (   (forall a. k a -> v a -> m (Compose ((,) (Seq (Event t w))) v' a))
                                            -> DMap k v
                                            -> Event t (p k v)
@@ -135,7 +135,9 @@ sequenceDMapWithAdjustEventWriterTWith base mapPatch weakenPatchWith patchNewEle
   childRequestMap :: Incremental t (p' (Some k) (Event t w)) <- holdIncremental requests0 requests'
   -- We add these two separately to take advantage of the free merge being done later.  The coincidence case must come first so that it has precedence if both fire simultaneously.  (Really, we should probably block the 'switch' whenever 'updated' fires, but switchPromptlyDyn has the same issue.)
   EventWriterT $ modify $ flip (|>) $ coincidence $ fforCheap requests' $ \p -> mconcat $ patchNewElements p --TODO: Create a mergeIncrementalPromptly, and use that to eliminate this 'coincidence'
-  EventWriterT $ modify $ flip (|>) $ fold <$> mergePatchIncremental childRequestMap
+  EventWriterT $ modify $ flip (|>) $ fforMaybeCheap (mergePatchIncremental childRequestMap) $ \m -> case toList m of
+    [] -> Nothing
+    h : t -> Just $ sconcat $ h :| t
   return (result0, result')
 
 instance PerformEvent t m => PerformEvent t (EventWriterT t w m) where
