4602fe7398ad4612b82ff61666c56f79bf648c65
Elliot Cameron <eacameron@gmail.com>
2018-05-09 18:48:41 -0400

    Add deprecated modules for Event-/DynamicWriter

---
+++ src/Reflex/DynamicWriter.hs
@@ -0,0 +1,7 @@
+module Reflex.DynamicWriter
+  {-# DEPRECATED "Use 'Reflex.DynamicWriter.Class' and 'Reflex.DynamicWrite.Base' instead, or just import 'Reflex'" #-}
+  ( module X
+  ) where
+
+import Reflex.DynamicWriter.Base as X
+import Reflex.DynamicWriter.Class as X
---
+++ src/Reflex/EventWriter.hs
@@ -0,0 +1,7 @@
+module Reflex.EventWriter
+  {-# DEPRECATED "Use 'Reflex.EventWriter.Class' and 'Reflex.EventWriter.Base' instead, or just import 'Reflex'" #-}
+  ( module X
+  ) where
+
+import Reflex.EventWriter.Base as X
+import Reflex.EventWriter.Class as X
