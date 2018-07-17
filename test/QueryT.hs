31aa1b0edd75dc8ff18cec7f4818b2746fe8584f
Ken Micklas <ken.micklas@obsidian.systems>
2018-07-17 14:31:24 -0400

    random queryT test stuff

--- test/QueryT.hs
+++ test/QueryT.hs
@@ -3,13 +3,18 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 
 import Control.Lens
+import Control.Monad
 import Control.Monad.Fix
+import Control.Monad.Reader
+import Control.Monad.State.Strict
 import Data.Align
 import Data.AppendMap () -- for the Align instance
 import qualified Data.AppendMap as AMap
+import Data.Foldable
 import Data.Functor.Misc
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -56,30 +61,91 @@ instance (Eq a, Ord k, Group a) => Additive (Selector k a)
 
 main :: IO ()
 main = do
-  [0, 1, 1, 0] <- fmap (map fst . concat) $
+  [[0], [1], [1], [0]] <- fmap (map (AMap.keys . unSelector . fst) . concat) $
     runApp (testQueryT testRunWithReplace) () $ map (Just . That) $
       [ That (), This (), That () ]
-  [0, 1, 1, 0] <- fmap (map fst . concat) $
+  result <-
+    runApp (testQueryT testRunWithReplace2) () $ map (Just . That) $
+      join $ replicate 5 [ That (), This $ This (), This $ That () ]
+  mapM_ print result
+  [[0], [1], [1], [0]] <- fmap (map (AMap.keys . unSelector . fst) . concat) $
     runApp (testQueryT testSequenceDMapWithAdjust) () $ map (Just . That) $
       [ That (), This (), That () ]
-  [0, 1, 1, 0] <- fmap (map fst . concat) $
+  [[0], [1], [1], [0]] <- fmap (map (AMap.keys . unSelector . fst) . concat) $
     runApp (testQueryT testSequenceDMapWithAdjustWithMove) () $ map (Just . That) $
       [ That (), This (), That () ]
   return ()
 
-testQueryT :: (Reflex t, MonadFix m)
-           => (Event t () -> Event t () -> QueryT t (Selector Int MyQuery) m ())
-           -> AppIn t () (These () ())
-           -> m (AppOut t Int Int)
+testQueryT
+  :: ( Reflex t
+     , MonadFix m
+     , Group q
+     , Additive q
+     , Monoid (QueryResult q)
+     --, PerformEvent t m
+     --, MonadIO (Performable m)
+     , Show q
+     )
+  => (Event t a -> Event t b -> QueryT t q m ())
+  -> AppIn t () (These a b)
+  -> m (AppOut t q q)
 testQueryT w (AppIn _ pulse) = do
   let replace = fmapMaybe (^? here) pulse
       increment = fmapMaybe (^? there) pulse
-  (_, q) <- runQueryT (w replace increment) $ pure mempty
-  let qDyn = head . AMap.keys . unSelector <$> incrementalToDynamic q
+  (_, q, b) <- runQueryTrace (w replace increment) $ pure mempty
+  --performEvent_ $ ffor patch $ \p -> liftIO $ putStrLn $ "PATCH: " ++ show p
+  let qDyn = incrementalToDynamic q
   return $ AppOut
-    { _appOut_behavior = current qDyn
+    { _appOut_behavior = b
     , _appOut_event = updated qDyn
     }
+
+runQueryTrace :: (MonadFix m, Additive q, Group q, Show q, Reflex t) => QueryT t q m a -> Dynamic t (QueryResult q) -> m (a, Incremental t (AdditivePatch q), Behavior t q)
+runQueryTrace (QueryT a) qr = do
+  ((r, bs), es) <- runReaderT (runEventWriterT (runStateT a mempty)) qr
+  let b = (foldlM (\b c -> (b <>) <$> sample c) mempty bs)
+  return (r, unsafeBuildIncremental b (fmapCheap AdditivePatch $ traceEvent "PATCH" es), pull b)
+
+--queryDynTrace :: (Reflex t, Monad m, MonadQuery t q m) => Dynamic t q -> m (Dynamic t (QueryResult q))
+--queryDynTrace q = do
+--  tellQueryDynTrace q
+--  zipDynWith crop q <$> askQueryResult
+--
+--tellQueryDynTrace :: (Reflex t, MonadQuery t q m) => Dynamic t q -> m ()
+--tellQueryDynTrace d = tellQueryIncrementalTrace $ unsafeBuildIncremental (sample (current d)) $ attachWith (\old new -> AdditivePatch $ new ~~ old) (current d) (updated d)
+--
+--tellQueryIncrementalTrace q = do
+--  QueryT (modify (currentIncremental q:))
+--  QueryT (lift (tellEvent (fmapCheap unAdditivePatch (updatedIncremental q))))
+
+runWithReplaceQueryT
+  :: forall t q m a b
+  . ( Reflex t
+    , MonadFix m
+    , Group q
+    , Additive q
+    , Query q
+    , MonadHold t m
+    , Adjustable t m
+    )
+  => QueryT t q m a
+  -> Event t (QueryT t q m b)
+  -> QueryT t q m (a, Event t b)
+runWithReplaceQueryT (QueryT a0) a' = do
+  bs <- QueryT get
+  ((r0, bs0), r') <- QueryT $ lift $ runWithReplace (runStateT a0 bs) $ fmapCheap (flip runStateT bs . unQueryT) a'
+  let sampleBs :: forall m'. MonadSample t m' => [Behavior t q] -> m' q
+      sampleBs = foldlM (\b a -> (b <>) <$> sample a) mempty
+      bs' = fmapCheap snd $ r'
+  bbs <- hold bs0 bs'
+  let patches = flip pushAlwaysCheap bs' $ \newBs -> do
+        oldBs <- sample bbs
+        (~~) <$> sampleBs newBs <*> sampleBs oldBs
+  QueryT $ modify $ (:) $ pull $ sampleBs =<< sample bbs
+  --QueryT $ put $ pure $ pull $ sampleBs =<< sample bbs
+  QueryT $ lift $ tellEvent patches
+  --QueryT $ lift $ tellEvent $ flip pushAlwaysCheap bs' $ \newBs -> sampleBs newBs
+  return (r0, fmapCheap fst r')
 
 testRunWithReplace :: ( Reflex t
                       , Adjustable t m
@@ -95,6 +161,28 @@ testRunWithReplace replace increment = do
         queryDyn $ zipDynWith (\x y -> Selector (AMap.singleton (x :: Int) y)) n $ pure $ MyQuery $ SelectedCount 1
   _ <- runWithReplace w $ w <$ replace
   return ()
+
+testRunWithReplace2
+  :: forall t m
+  .  ( Reflex t
+     , MonadFix m
+     , MonadHold t m
+     , Adjustable t m
+     --, MonadQuery t MyQuery m
+     )
+  => Event t (These () ())
+  -> Event t ()
+  -> QueryT t MyQuery m ()
+testRunWithReplace2 pb tick = do
+  let pbOuter = fmapMaybe (^? here) pb
+  let pbInner = fmapMaybe (^? there) pb
+  void $ runWithReplaceQueryT (return ()) $ ffor tick $ const $ do
+    void $ runWithReplaceQueryT (return ()) $ ffor (leftmost [pbOuter, tick]) $ const $ do
+      void $ runWithReplaceQueryT (return ()) $ ffor (leftmost [pbInner, tick]) $ const $ do
+        --void $ queryDyn $ pure $ MyQuery $ SelectedCount 1
+        let q :: Incremental t (AdditivePatch MyQuery) = unsafeBuildIncremental (pure $ MyQuery $ SelectedCount 1 :: PullM t MyQuery) never
+        QueryT (modify (currentIncremental q:))
+        --QueryT (lift (tellEvent (fmapCheap unAdditivePatch (updatedIncremental q))))
 
 testSequenceDMapWithAdjust :: ( Reflex t
                               , Adjustable t m
