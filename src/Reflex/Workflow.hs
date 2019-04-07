56bdb1c7fda5d9fe7a5a50a3772cc14e26ede109
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2019-04-07 22:28:29 +0100

    Add common instances for 'Workflow'

--- src/Reflex/Workflow.hs
+++ src/Reflex/Workflow.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 -- |
@@ -6,17 +9,24 @@
 -- Description:
 --   Provides a convenient way to describe a series of interrelated widgets that
 --   can send data to, invoke, and replace one another. Useful for modeling user interface
---   "workflows."
+--   "workflows".
 module Reflex.Workflow (
     Workflow (..)
   , workflow
   , workflowView
   , mapWorkflow
   , mapWorkflowCheap
+  , parallelWorkflows
+  , zipWorkflows
   ) where
 
-import Control.Arrow ((***))
+import Control.Arrow (first, (***))
 import Control.Monad.Fix (MonadFix)
+
+import Data.Align
+import Data.These
+import Data.Functor.Bind
+import Data.Functor.Plus
 
 import Reflex.Class
 import Reflex.Adjustable.Class
@@ -26,7 +36,48 @@ import Reflex.PostBuild.Class
 
 -- | A widget in a workflow
 -- When the 'Event' returned by a 'Workflow' fires, the current 'Workflow' is replaced by the one inside the firing 'Event'. A series of 'Workflow's must share the same return type.
-newtype Workflow t m a = Workflow { unWorkflow :: m (a, Event t (Workflow t m a)) }
+newtype Workflow t m a = Workflow { unWorkflow :: m (a, Event t (Workflow t m a)) } deriving Functor
+
+instance (Apply m, Reflex t) => Apply (Workflow t m) where
+  liftF2 f = parallelWorkflows f f f
+
+instance (Apply m, Applicative m, Reflex t) => Applicative (Workflow t m) where
+  pure a = Workflow $ pure (a, never)
+  (<*>) = (<.>)
+
+instance (Apply m, Reflex t, Semigroup a) => Semigroup (Workflow t m a) where
+  (<>) = liftF2 (<>)
+
+instance (Apply m, Applicative m, Reflex t, Monoid a) => Monoid (Workflow t m a) where
+  mempty = pure mempty
+
+instance (Apply m, Reflex t) => Alt (Workflow t m) where
+  (<!>) = parallelWorkflows const (flip const) const
+
+zipWorkflows :: (Apply m, Reflex t) => Workflow t m a -> Workflow t m b -> Workflow t m (a,b)
+zipWorkflows = parallelWorkflows (,) (,) (,)
+
+parallelWorkflows :: (Apply m, Reflex t)
+                  => (a -> b -> c) -> (a -> b -> c) -> (a -> b -> c)
+                  -> Workflow t m a -> Workflow t m b -> Workflow t m c
+parallelWorkflows fL fR fLR = go fLR
+  where
+    go f0 wl wr = Workflow $ ffor2 (unWorkflow wl) (unWorkflow wr) $ \(l0, wlEv) (r0, wrEv) ->
+      (f0 l0 r0, ffor (align wlEv wrEv) $ \case
+          This wl' -> go fL wl' wr
+          That wr' -> go fR wl wr'
+          These wl' wr' -> go fLR wl' wr'
+      )
+
+instance (Apply m, Monad m, Reflex t) => Bind (Workflow t m) where
+  join wwa = Workflow $ do
+    let replaceInitial a wa = Workflow $ first (const a) <$> unWorkflow wa
+    (wa0, wwaEv) <- unWorkflow wwa
+    (a0, waEv) <- unWorkflow wa0
+    pure (a0, join <$> leftmost [wwaEv, flip replaceInitial wwa <$> waEv])
+
+instance (Apply m, Monad m, Reflex t) => Monad (Workflow t m) where
+  (>>=) = (>>-)
 
 -- | Runs a 'Workflow' and returns the 'Dynamic' result of the 'Workflow' (i.e., a 'Dynamic' of the value produced by the current 'Workflow' node, and whose update 'Event' fires whenever one 'Workflow' is replaced by another).
 workflow :: forall t m a. (Reflex t, Adjustable t m, MonadFix m, MonadHold t m) => Workflow t m a -> m (Dynamic t a)
@@ -41,9 +92,10 @@ workflowView w0 = do
       eReplace <- fmap switch $ hold never $ fmap snd eResult
   return $ fmap fst eResult
 
--- | Map a function over a 'Workflow', possibly changing the resturn type.
+{-# DEPRECATED mapWorkflow "Use 'fmap' instead" #-}
+-- | Map a function over a 'Workflow', possibly changing the return type.
 mapWorkflow :: (Reflex t, Functor m) => (a -> b) -> Workflow t m a -> Workflow t m b
-mapWorkflow f (Workflow x) = Workflow (fmap (f *** fmap (mapWorkflow f)) x)
+mapWorkflow = fmap
 
 -- | Map a "cheap" function over a 'Workflow'. Refer to the documentation for 'pushCheap' for more information and performance considerations.
 mapWorkflowCheap :: (Reflex t, Functor m) => (a -> b) -> Workflow t m a -> Workflow t m b
