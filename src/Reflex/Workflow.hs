7e33368655c31a91fb55fd0b4a06d5943f8cfac9
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2019-05-27 09:52:32 +0100

    Group by sections

--- src/Reflex/Workflow.hs
+++ src/Reflex/Workflow.hs
@@ -48,18 +48,67 @@ import Reflex.PostBuild.Class
 -- When the 'Event' returned by a 'Workflow' fires, the current 'Workflow' is replaced by the one inside the firing 'Event'. A series of 'Workflow's must share the same return type.
 newtype Workflow t m a = Workflow { unWorkflow :: m (a, Event t (Workflow t m a)) } deriving Functor
 
+--------------------------------------------------------------------------------
+-- Running workflows
+--------------------------------------------------------------------------------
+-- | Runs a 'Workflow' and returns the 'Dynamic' result of the 'Workflow' (i.e., a 'Dynamic' of the value produced by the current 'Workflow' node, and whose update 'Event' fires whenever one 'Workflow' is replaced by another).
+workflow :: forall t m a. (Reflex t, Adjustable t m, MonadFix m, MonadHold t m) => Workflow t m a -> m (Dynamic t a)
+workflow w0 = do
+  rec eResult <- networkHold (unWorkflow w0) $ fmap unWorkflow $ switch $ snd <$> current eResult
+  return $ fmap fst eResult
+
+-- | Similar to 'workflow', but outputs an 'Event' that fires whenever the current 'Workflow' is replaced by the next 'Workflow'.
+workflowView :: forall t m a. (Reflex t, NotReady t m, Adjustable t m, MonadFix m, MonadHold t m, PostBuild t m) => Workflow t m a -> m (Event t a)
+workflowView w0 = do
+  rec eResult <- networkView . fmap unWorkflow =<< holdDyn w0 eReplace
+      eReplace <- fmap switch $ hold never $ fmap snd eResult
+  return $ fmap fst eResult
+
+--------------------------------------------------------------------------------
+-- Transforming workflows
+--------------------------------------------------------------------------------
+{-# DEPRECATED mapWorkflow "Use 'fmap' instead" #-}
+-- | Map a function over a 'Workflow', possibly changing the return type.
+mapWorkflow :: (Reflex t, Functor m) => (a -> b) -> Workflow t m a -> Workflow t m b
+mapWorkflow = fmap
+
+-- | Map a "cheap" function over a 'Workflow'. Refer to the documentation for 'pushCheap' for more information and performance considerations.
+mapWorkflowCheap :: (Reflex t, Functor m) => (a -> b) -> Workflow t m a -> Workflow t m b
+mapWorkflowCheap f (Workflow x) = Workflow (fmap (f *** fmapCheap (mapWorkflowCheap f)) x)
+
+zipNEListWithWorkflow :: (Functor m, Reflex t) => NonEmpty k -> Workflow t m a -> Workflow t m (k, a)
+zipNEListWithWorkflow (k :| ks) w = Workflow $ ffor (unWorkflow w) $ \(a0, wEv) ->
+  ((k, a0), case nonEmpty ks of
+      Nothing -> never
+      Just nel -> zipNEListWithWorkflow nel <$> wEv)
+
 instance (Functor m, Reflex t) => FunctorWithIndex Int (Workflow t m) where
   imap f w = uncurry f <$> zipNEListWithWorkflow (0 :| [1..]) w
 
--- | Create a workflow that's replaced when either input workflow is replaced.
--- Occurrences of the left workflow cause the right workflow to be reset
-instance (Apply m, Reflex t) => Apply (Workflow t m) where
-  liftF2 f = chainWorkflows (const $ uncurry f) zipWidgets
+--------------------------------------------------------------------------------
+-- Combining payloads
+--------------------------------------------------------------------------------
+thesePayloads :: ((a,b) -> c) -> ((a,b) -> c) -> ((a,b) -> c) -> These () () -> (a,b) -> c
+thesePayloads fa fb fab = \case
+  This () -> fa
+  That () -> fb
+  These () () -> fab
 
-instance (Apply m, Applicative m, Reflex t) => Applicative (Workflow t m) where
-  pure a = Workflow $ pure (a, never)
-  (<*>) = (<.>)
+leftBiasedLastOccurrence :: These () () -> (a,a) -> a
+leftBiasedLastOccurrence = thesePayloads fst snd fst
 
+ignoreTimings :: ((a,b) -> c) -> These () () -> (a,b) -> c
+ignoreTimings = const
+
+--------------------------------------------------------------------------------
+-- Combining widgets
+--------------------------------------------------------------------------------
+zipWidgets :: Apply f => (f a, f b) -> f (a,b)
+zipWidgets (a,b) = liftF2 (,) a b
+
+--------------------------------------------------------------------------------
+-- Combining workflows
+--------------------------------------------------------------------------------
 instance (Apply m, Reflex t, Semigroup a) => Semigroup (Workflow t m a) where
   (<>) = zipWorkflowsWith (<>)
 
@@ -76,40 +125,14 @@ instance (Apply m, Reflex t) => Semialign (Workflow t m) where
   align = parallelWorkflows (This . fst) (That . snd) (uncurry These) zip
 #endif
 
--- | Collapse a workflow of workflows into one level
--- Whenever both outer and inner workflows are replaced at the same time, the inner one is ignored
-instance (Apply m, Monad m, Reflex t) => Bind (Workflow t m) where
-  join wwa = Workflow $ do
-    let replaceInitial a wa = Workflow $ first (const a) <$> unWorkflow wa
-    (wa0, wwaEv) <- unWorkflow wwa
-    (a0, waEv) <- unWorkflow wa0
-    pure (a0, join <$> leftmost [wwaEv, flip replaceInitial wwa <$> waEv])
+-- | Create a workflow that's replaced when either input workflow is replaced.
+-- Occurrences of the left workflow cause the right workflow to be reset
+instance (Apply m, Reflex t) => Apply (Workflow t m) where
+  liftF2 f = chainWorkflows (const $ uncurry f) zipWidgets
 
-instance (Apply m, Monad m, Reflex t) => Monad (Workflow t m) where
-  (>>=) = (>>-)
-
-
-zipWidgets :: Apply f => (f a, f b) -> f (a,b)
-zipWidgets (a,b) = liftF2 (,) a b
-
-thesePayloads :: ((a,b) -> c) -> ((a,b) -> c) -> ((a,b) -> c) -> These () () -> (a,b) -> c
-thesePayloads fa fb fab = \case
-  This () -> fa
-  That () -> fb
-  These () () -> fab
-
-leftBiasedLastOccurrence :: These () () -> (a,a) -> a
-leftBiasedLastOccurrence = thesePayloads fst snd fst
-
-ignoreTimings :: ((a,b) -> c) -> These () () -> (a,b) -> c
-ignoreTimings = const
-
-zipNEListWithWorkflow :: (Functor m, Reflex t) => NonEmpty k -> Workflow t m a -> Workflow t m (k, a)
-zipNEListWithWorkflow (k :| ks) w = Workflow $ ffor (unWorkflow w) $ \(a0, wEv) ->
-  ((k, a0), case nonEmpty ks of
-      Nothing -> never
-      Just nel -> zipNEListWithWorkflow nel <$> wEv)
-
+instance (Apply m, Applicative m, Reflex t) => Applicative (Workflow t m) where
+  pure a = Workflow $ pure (a, never)
+  (<*>) = (<.>)
 
 -- | Combine two workflows via `combineWorkflows`. Triggers of the first workflow reset the second one.
 chainWorkflows
@@ -165,25 +188,17 @@ combineWorkflows combineP combineW trigger wl0 wr0 = go (These () ()) (wl0, wr0)
              These wl' wr' -> go (These () ()) (t (These wl' wr'))
          )
 
+--------------------------------------------------------------------------------
+-- Flattening workflows
+--------------------------------------------------------------------------------
+-- | Collapse a workflow of workflows into one level
+-- Whenever both outer and inner workflows are replaced at the same time, the inner one is ignored
+instance (Apply m, Monad m, Reflex t) => Bind (Workflow t m) where
+  join wwa = Workflow $ do
+    let replaceInitial a wa = Workflow $ first (const a) <$> unWorkflow wa
+    (wa0, wwaEv) <- unWorkflow wwa
+    (a0, waEv) <- unWorkflow wa0
+    pure (a0, join <$> leftmost [wwaEv, flip replaceInitial wwa <$> waEv])
 
--- | Runs a 'Workflow' and returns the 'Dynamic' result of the 'Workflow' (i.e., a 'Dynamic' of the value produced by the current 'Workflow' node, and whose update 'Event' fires whenever one 'Workflow' is replaced by another).
-workflow :: forall t m a. (Reflex t, Adjustable t m, MonadFix m, MonadHold t m) => Workflow t m a -> m (Dynamic t a)
-workflow w0 = do
-  rec eResult <- networkHold (unWorkflow w0) $ fmap unWorkflow $ switch $ snd <$> current eResult
-  return $ fmap fst eResult
-
--- | Similar to 'workflow', but outputs an 'Event' that fires whenever the current 'Workflow' is replaced by the next 'Workflow'.
-workflowView :: forall t m a. (Reflex t, NotReady t m, Adjustable t m, MonadFix m, MonadHold t m, PostBuild t m) => Workflow t m a -> m (Event t a)
-workflowView w0 = do
-  rec eResult <- networkView . fmap unWorkflow =<< holdDyn w0 eReplace
-      eReplace <- fmap switch $ hold never $ fmap snd eResult
-  return $ fmap fst eResult
-
-{-# DEPRECATED mapWorkflow "Use 'fmap' instead" #-}
--- | Map a function over a 'Workflow', possibly changing the return type.
-mapWorkflow :: (Reflex t, Functor m) => (a -> b) -> Workflow t m a -> Workflow t m b
-mapWorkflow = fmap
-
--- | Map a "cheap" function over a 'Workflow'. Refer to the documentation for 'pushCheap' for more information and performance considerations.
-mapWorkflowCheap :: (Reflex t, Functor m) => (a -> b) -> Workflow t m a -> Workflow t m b
-mapWorkflowCheap f (Workflow x) = Workflow (fmap (f *** fmapCheap (mapWorkflowCheap f)) x)
+instance (Apply m, Monad m, Reflex t) => Monad (Workflow t m) where
+  (>>=) = (>>-)
