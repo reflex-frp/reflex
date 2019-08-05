72d602fcde474682c677158294122e1291c5b184
Ali Abrar <aliabrar@gmail.com>
2019-08-04 23:15:24 -0400

    Bump version to 0.6.2.2; Add flag to choose whether to use new split these package

--- default.nix
+++ default.nix
@@ -7,11 +7,12 @@
 , transformers-compat, unbounded-delays, prim-uniq
 , data-default, filepath, directory, filemanip, ghcjs-base
 , monoidal-containers, witherable
+, semialign ? null, splitThese ? true
 , useTemplateHaskell ? true
 }:
 mkDerivation {
   pname = "reflex";
-  version = "0.6.2.1";
+  version = "0.6.2.2";
   src = builtins.filterSource (path: type: !(builtins.elem (baseNameOf path) [ ".git" "dist" ])) ./.;
   libraryHaskellDepends = [
     base bifunctors containers dependent-map dependent-sum
@@ -26,13 +27,15 @@ mkDerivation {
     ghcjs-base
   ] else []) ++ (if !useTemplateHaskell then [] else [
     haskell-src-exts haskell-src-meta
-  ]);
+  ]) ++ (if splitThese then [
+    semialign
+  ] else []);
   testHaskellDepends = if ghc.isGhcjs or false then [] else [
     hlint filepath directory filemanip
   ];
-  configureFlags = if useTemplateHaskell then [] else [
-    "-f-use-template-haskell"
-  ];
+  configureFlags =
+    stdenv.lib.optional (!useTemplateHaskell) [ "-f-use-template-haskell" ] ++
+    stdenv.lib.optional (!splitThese) [ "-f-split-these" ];
   homepage = "https://github.com/reflex-frp/reflex";
   description = "Higher-order Functional Reactive Programming";
   license = stdenv.lib.licenses.bsd3;
