98a66d77116ff50c3127255973d3bd160cea7b3a
John Ericson <git@JohnEricson.me>
2019-11-13 23:12:32 -0500

    Modernize release.nix using way newer reflex-platform,

--- release.nix
+++ release.nix
@@ -1,9 +1,38 @@
-{ rp ? import ./dep/reflex-platform {}
+{ reflex-platform-fun ? import ./dep/reflex-platform
 }:
+
 let
-  inherit (rp.nixpkgs) lib;
-  compilers = ["ghc8_4" "ghc8_0" "ghcjs8_4" "ghcjs8_0"];
-in lib.attrValues (lib.genAttrs compilers (ghc: {
-  reflex-useTemplateHaskell = rp.${ghc}.callPackage ./. { useTemplateHaskell = true; splitThese = false; };
-  reflex = rp.${ghc}.callPackage ./. { useTemplateHaskell = false; splitThese = false; };
-}))
+  native-reflex-platform = reflex-platform-fun {};
+  inherit (native-reflex-platform.nixpkgs) lib;
+  systems = ["x86_64-linux" "x86_64-darwin"];
+
+  perPlatform = lib.genAttrs systems (system: let
+    reflex-platform = reflex-platform-fun { inherit system; };
+    compilers = [
+      "ghc"
+      "ghcjs"
+    ] ++ lib.optionals (reflex-platform.androidSupport) [
+      "ghcAndroidAarch64"
+      "ghcAndroidAarch32"
+    ] ++ lib.optionals (reflex-platform.iosSupport) [
+      "ghcIosAarch64"
+    ];
+    hsPkgs = lib.genAttrs compilers (ghc: let
+      ghc' = reflex-platform.${ghc}.override {
+        overrides = self: super: {
+          reflex-useTemplateHaskell = self.callPackage ./. { useTemplateHaskell = true; splitThese = false; };
+          reflex = self.callPackage ./. { useTemplateHaskell = false; splitThese = false; };
+        };
+      };
+    in {
+      inherit (ghc') reflex reflex-useTemplateHaskell;
+    });
+  in hsPkgs // {
+    cache = reflex-platform.pinBuildInputs "reflex-${system}"
+      (lib.concatLists (map builtins.attrValues (builtins.attrValues hsPkgs)));
+  });
+
+  metaCache = native-reflex-platform.pinBuildInputs "reflex-everywhere"
+    (map (a: a.cache) (builtins.attrValues perPlatform));
+
+in perPlatform // { inherit metaCache; }
