b432a091fb1de8d5899d997011f826770b843d25
Carlo Nucera <carlo.nucera@protonmail.com>
2020-04-30 11:09:21 -0400

    Add Reflex.Host.Headless

---
+++ src/Reflex/Host/Headless.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ConstraintKinds #-}
+
+module Reflex.Host.Headless where
+
+import Data.Maybe
+import Reflex
+import Control.Monad.Fix
+import Control.Monad.Primitive
+import Reflex.Host.Class
+import Control.Monad.IO.Class
+import Control.Monad.Ref
+import Data.IORef
+import Data.Dependent.Sum
+import Control.Concurrent.Chan (newChan, readChan)
+import Control.Monad (forM, forM_)
+import Control.Monad.Identity (Identity(..))
+
+type MonadHeadlessApp t m =
+  ( Reflex t
+  , MonadHold t m
+  , MonadFix m
+  , PrimMonad (HostFrame t)
+  , ReflexHost t
+  , MonadIO (HostFrame t)
+  , Ref m ~ IORef
+  , Ref (HostFrame t) ~ IORef
+  , MonadRef (HostFrame t)
+  , NotReady t m
+  , TriggerEvent t m
+  , PostBuild t m
+  , PerformEvent t m
+  , MonadIO m
+  , MonadIO (Performable m)
+  , Adjustable t m
+  )
+
+runHeadlessApp
+  :: (forall t m. MonadHeadlessApp t m => m (Event t ()))
+  -> IO ()
+runHeadlessApp guest =
+  (runSpiderHost :: SpiderHost Global a -> IO a) $ do
+    (postBuild, postBuildTriggerRef) <- newEventWithTriggerRef
+    events <- liftIO newChan
+    (result, fc@(FireCommand fire)) <- do
+      hostPerformEventT $
+        flip runPostBuildT postBuild $
+          flip runTriggerEventT events $
+            guest
+    mPostBuildTrigger <- readRef postBuildTriggerRef
+    forM_ mPostBuildTrigger $ \postBuildTrigger ->
+      fire [postBuildTrigger :=> Identity ()] $ return ()
+    shutdown <- subscribeEvent result
+    fix $ \loop -> do
+      ers <- liftIO $ readChan events
+      stop <- fireEventTriggerRefs fc ers $ readEvent shutdown >>= \case
+        Nothing -> return False
+        Just _ -> return True
+      if or stop
+        then return ()
+        else loop
+  where
+    -- | Use the given 'FireCommand' to fire events that have subscribers
+    -- and call the callback for the 'TriggerInvocation' of each.
+    fireEventTriggerRefs
+      :: (MonadIO m)
+      => FireCommand t m
+      -> [DSum (EventTriggerRef t) TriggerInvocation]
+      -> ReadPhase m a
+      -> m [a]
+    fireEventTriggerRefs (FireCommand fire) ers rcb = do
+      mes <- liftIO $
+        forM ers $ \(EventTriggerRef er :=> TriggerInvocation a _) -> do
+          me <- readIORef er
+          return $ fmap (\e -> e :=> Identity a) me
+      a <- fire (catMaybes mes) rcb
+      liftIO $ forM_ ers $ \(_ :=> TriggerInvocation _ cb) -> cb
+      return a
