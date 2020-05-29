306ae74e681f533f73f1f2ab1edde4f662caf90e
Ryan Trinkle <ryan@trinkle.org>
2020-05-28 20:02:29 -0400

    This hits the bug

--- test/nestedAdjustable.hs
+++ test/nestedAdjustable.hs
@@ -11,40 +11,60 @@ import Control.Monad.Fix (MonadFix)
 import Control.Monad (void)
 import Control.Concurrent
 import Control.Monad.IO.Class
+import Data.IORef
+import System.IO.Unsafe
+import Debug.Trace
 
 main :: IO ()
-main = runHeadlessApp $ do
-  (e, eTrigger) <- newTriggerEvent
-  mainWidget e
-  liftIO $ forkIO $ do
+main = runHeadlessApp mainWidget
+
+mainWidget
+  :: forall m t
+   . ( MonadFix m
+     , MonadHold t m
+     , PostBuild t m
+     , MonadIO m
+     , TriggerEvent t m
+     , NotReady t m
+     , Adjustable t m
+     )
+  => m (Event t ())
+mainWidget = do
+  ref                   <- liftIO $ newIORef 0
+  (e    , eTrigger    ) <- newTriggerEvent
+  (close, closeTrigger) <- newTriggerEvent
+  void $ liftIO $ forkIO $ do
     threadDelay 1000000
     eTrigger ()
-  pure never
+    threadDelay 1000000
+    count <- readIORef ref
+    if count == 1
+      then do
+        putStrLn
+          "It appears there is no bug here! Trigger was counted exactly once!"
+        closeTrigger ()
+      else do
+        putStrLn $ "Bug! Triggers counted: " <> show count
+        closeTrigger ()
+  dynamic <- countTriggers ref <$> holdDyn () e
+  void $ simpleList (constDyn [0 .. bugFactor]) $ const
+    (void $ networkView $ pure () <$ dynamic)
+  pure close
 
-mainWidget :: (MonadFix m, PostBuild t m, MonadHold t m, NotReady t m, Adjustable t m) => Event t () -> m ()
-mainWidget e = do
-  rec let (appChangeEvents, _) = fanThese $ partitionEithersNE <$> stateChanges
-      dragDyn <- holdDyn () $ NonEmpty.last <$> appChangeEvents
-      (_, stateChanges' :: Event t (NonEmpty (Either () ()))) <-
-        runEventWriterT
-          (do
-            tellEvent . fmap (:| [])
-               . fmap (\_ -> Right ()) $ traceEventWith (const "Creating Task2") e
-            void $ networkView $ (void $ networkView $ pure () <$ dragDyn) <$ pure ()
-            void $ networkView $ (void $ networkView $ pure () <$ dragDyn) <$ pure ()
-            void $ networkView $ (void $ networkView $ pure () <$ dragDyn) <$ pure ()
-            void $ networkView $ (void $ networkView $ pure () <$ dragDyn) <$ pure ()
-            void $ networkView $ (void $ networkView $ pure () <$ dragDyn) <$ pure ()
-            void $ networkView $ (void $ networkView $ pure () <$ dragDyn) <$ pure ()
-            void $ networkView $ (void $ networkView $ pure () <$ dragDyn) <$ pure ()
-          )
-      stateChanges <- pure $ traceEventWith (const "StateChange") stateChanges'
-  pure ()
+bugFactor :: Int
+bugFactor = 1000
 
-partitionEithersNE :: NonEmpty (Either a b) -> These (NonEmpty a) (NonEmpty b)
-partitionEithersNE (x :| xs) = case (x, ls, rs) of
-  (Left  y, ys    , []    ) -> This (y :| ys)
-  (Left  y, ys    , z : zs) -> These (y :| ys) (z :| zs)
-  (Right z, []    , zs    ) -> That (z :| zs)
-  (Right z, y : ys, zs    ) -> These (y :| ys) (z :| zs)
-  where (ls, rs) = partitionEithers xs
+{-# NOINLINE incrementRef #-}
+incrementRef :: (Show a) => IORef Int -> a -> String
+incrementRef ref x = unsafePerformIO
+  (atomicModifyIORef
+    ref
+    (\a -> (a + 1, show x <> " triggered time: " <> show (a + 1)))
+  )
+
+countTriggers
+  :: (Reflex t, Show a) => IORef Int -> Dynamic t a -> Dynamic t a
+countTriggers ref d =
+  let e'    = traceEventWith (incrementRef ref) $ updated d
+      getV0 = sample $ current d
+  in  trace "Registering counter" $ unsafeBuildDynamic getV0 e'
