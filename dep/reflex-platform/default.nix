f2296c8071073348e98a0a77b2bfa0c0f788db1c
Ali Abrar <ali.abrar@obsidian.systems>
2022-07-01 10:26:04 -0400

    ci: Update dependencies

--- dep/reflex-platform/default.nix
+++ dep/reflex-platform/default.nix
@@ -1,7 +1,2 @@
 # DO NOT HAND-EDIT THIS FILE
-import ((import <nixpkgs> {}).fetchFromGitHub (
-  let json = builtins.fromJSON (builtins.readFile ./github.json);
-  in { inherit (json) owner repo rev sha256;
-       private = json.private or false;
-     }
-))
+import (import ./thunk.nix)
