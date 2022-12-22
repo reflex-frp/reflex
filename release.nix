82c2b5ef5f9ab390545756132771f91b4aafbfd1
Ali Abrar <aliabrar@gmail.com>
2022-12-21 22:32:53 -0400

    Support ghc 9.4.3 (#486)
    * ci: enable ghc 9.4.3
    
    * cabal: loosen bounds
    
    * ci: remove extra cabal update
    
    * Add shell.nix; Allow compiler selection when entering shell
    
    * ci: update github cache action
    
    * cabal: loosen version bounds; Improve shell.nix
    
    * cabal: loosen time bounds
    
    * ci: fix reflex-platform build

--- release.nix
+++ release.nix
@@ -3,11 +3,11 @@
 }:
 
 let
-  native-reflex-platform = reflex-platform-fun {};
+  native-reflex-platform = reflex-platform-fun { __useNewerCompiler = true; };
   inherit (native-reflex-platform.nixpkgs) lib;
 
   perPlatform = lib.genAttrs supportedSystems (system: let
-    reflex-platform = reflex-platform-fun { inherit system; };
+    reflex-platform = reflex-platform-fun { inherit system;  __useNewerCompiler = true; };
     compilers = [
       "ghc"
       "ghcjs"
@@ -25,32 +25,13 @@ let
       variationPkgs = lib.genAttrs variations (variation: let
         reflex-platform = reflex-platform-fun {
           inherit system;
+          __useNewerCompiler = true;
           __useTemplateHaskell = variation == "reflex"; # TODO hack
           haskellOverlays = [
-            (self: super: {
-              commutative-semigroups = self.callHackageDirect {
-                pkg = "commutative-semigroups";
-                ver = "0.1.0.0";
-                sha256 = "0xmv20n3iqjc64xi3c91bwqrg8x79sgipmflmk21zz4rj9jdkv8i";
-              } {};
-              patch = self.callHackageDirect {
-                pkg = "patch";
-                ver = "0.0.8.0";
-                sha256 = "1nnp7jn0vbx9zrnf57dxbknp6fbkqz7bca4i40aa6fabpwjw97kg";
-              } {};
-            })
+            (self: super: import ./overlay.nix { inherit self super; haskellLib = native-reflex-platform.nixpkgs.haskell.lib; })
             # Use this package's source for reflex
             (self: super: {
-              _dep = super._dep // {
-                reflex = builtins.filterSource (path: type: !(builtins.elem (baseNameOf path) [
-                  "release.nix"
-                  ".git"
-                  "dist"
-                  "cabal.haskell-ci"
-                  "cabal.project"
-                  ".travis.yml"
-                ])) ./.;
-              };
+              _dep = super._dep // { reflex = import ./src.nix; };
             })
           ];
         };
